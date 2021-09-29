83f8901d1e9edaf0ebbd600848f6fd423830ae71
Dima Tytenko <amgpetronass@gmail.com>
2021-09-29 21:00:45 +0300

    создал меню по шаблону

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './sass/main.scss';
+import cards from './menu.json';
+import itemsTemplate from './gallery-items.hbs';
+
+const galleryRef = document.querySelector('.js-menu');
+
+const markup = itemsTemplate(cards);
+galleryRef.insertAdjacentHTML('beforeend', markup);
