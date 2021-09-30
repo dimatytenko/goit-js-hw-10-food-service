4873595b7b94d06922c755d21828811dbba766cc
Dima Tytenko <amgpetronass@gmail.com>
2021-09-30 20:10:13 +0300

    add change themes

--- src/change-themes.js
+++ src/change-themes.js
@@ -1,17 +1,31 @@
 const inputEl = document.querySelector('.theme-switch__toggle');
 const bodyEl = document.querySelector('body');
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 const { LIGHT, DARK } = Theme;
 
-bodyEl.classList.add(LIGHT);
-localStorage.setItem('theme', LIGHT);
+abracadabra();
 
 inputEl.addEventListener('change', onThemeChange);
 
 function onThemeChange(event) {
-  bodyEl.classList.toggle(LIGHT);
-  bodyEl.classList.toggle(DARK);
+  if (bodyEl.classList.contains(LIGHT)) {
+    bodyEl.classList.replace(LIGHT, DARK);
+    localStorage.setItem('theme', DARK);
+  } else {
+    bodyEl.classList.replace(DARK, LIGHT);
+    localStorage.setItem('theme', LIGHT);
+  }
+}
+function abracadabra() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === DARK) {
+    bodyEl.classList.add(savedTheme);
+    inputEl.checked = true;
+  } else {
+    bodyEl.classList.add(LIGHT);
+  }
 }
